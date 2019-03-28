725fa26fd4d6e4ba9ca1b963c1fb8d01c67c88bf
watcherlab <watcherlab@watcherlab.com>
2019-03-28 21:44:13 +0800

    token

--- src/main/java/com/watcherlab/client/AdvancedQuery.java
+++ src/main/java/com/watcherlab/client/AdvancedQuery.java
@@ -18,7 +18,7 @@ public class AdvancedQuery {
      * 请求token，注册登陆后在用户中心获取,格式：41eec916c3b46e41e9457ba9525dddbdd15d35f422e102b6fa12768c2c9fffff，
      * 注册地址：https://feed.watcherlab.com/#/user/register
      * */
-    private static final String MY_TOKEN = "41eec916c3b46e41e9457ba9525dddbdd15d35f422e102b6fa12768c2c9cc0ac";
+    private static final String MY_TOKEN = "";
 
     /**
      * 高级查询接口客户端请求方法示例
--- src/main/java/com/watcherlab/client/DownloadAdvanced.java
+++ src/main/java/com/watcherlab/client/DownloadAdvanced.java
@@ -22,7 +22,7 @@ public class DownloadAdvanced {
     /** 接口请求地址 */
     private static final String DOWNLOAD_URL = "https://feed.watcherlab.com/api/download/advanced";
     /** 请求token，注册登陆后在用户中心获取，注册地址：https://feed.watcherlab.com/#/user/register */
-    private static final String MY_TOKEN = "41eec916c3b46e41e9457ba9525dddbdd15d35f422e102b6fa12768c2c9cc0ac";
+    private static final String MY_TOKEN = "";
     /** 下载时间，需要指定 */
     private static final String DATE_STR = "20190324";
 
